86cb251320cec02740b172a38e9ec0679e8240e9
alu3875 <diegoaguilar.88@gmail.com>
2013-11-19 22:44:21 +0000

    Modificando gitignore para ~

--- linkjobs/urls.py
+++ linkjobs/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$','home_view',name='vista_home'),
+    #url(r'^$','home_view'),
 
     # Examples:
     # url(r'^$', 'linkjobs.views.home', name='home'),
