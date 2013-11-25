4c952abfb5c53b83344f882a0e5490f72f4ac611
Javier Mena Mena <alu0100454741@ull.edu.es>
2013-11-25 16:45:10 +0000

    Añadiendo ruta en url del about

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('home.views',
     url(r'^$','home_view',name='vista_home'),
+    url(r'^about/$','about_view',name='vista_about'),
+
+    url (r'^signup/$','signup',name='vista_signup'),
 )
