d6a267be7252594b452b9a242c19a0c798c3b3da
Diego Williams Aguilar Montaño <alu0100592368@ull.edu.es>
2013-11-25 22:14:58 +0000

    modificando urls.py

--- home/urls.py
+++ home/urls.py
@@ -6,7 +6,6 @@ urlpatterns = patterns('home.views',
     url(r'^about/$','about_view',name='vista_about'),
     url(r'^privacity/$','privacity_view',name='vista_priva'),
     url(r'^terms_of_use/$','terms_of_use_view',name='vista_terminos'),
-
     url (r'^signup/$','signup',name='vista_signup'),
     url (r'^login/$','login',name='vista_login'),
 
