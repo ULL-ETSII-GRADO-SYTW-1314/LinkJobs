de5bd458eb7f095dfc85171e557a503b3205f2d3
Javier Mena Mena <alu0100454741@ull.edu.es>
2013-11-25 16:59:20 +0000

    Añadiendo modelos para la base de datos

--- home/models.py
+++ home/models.py
@@ -1,4 +1,32 @@
 from django.db import models
+from time import time
+import re
+ 
+class User(models.Model):
+	nombre		= models.CharField(max_length=30)
+	apellidos	= models.CharField(max_length=80)
+	username	= models.CharField(max_length=30)
+	email1		= models.EmailField(max_length=70)
+	curriculum	= models.CharField(max_length=600)
+	profesion	= models.CharField(max_length= 30)
+	password1	= models.CharField(max_length=30)
+	photo 		= models.ImageField(upload_to=get_upload_image, default='img/pic4.jpg',blank=True, null=True)
 
-#Hola
-# Create your models here.
+
+	def insertar(self, a, b, c, d, e):
+	    self.nombre = a
+	    self.apellidos = b
+	    self.username = c
+	    self.email1 = d
+	    self.password1 = e
+
+
+	def InsertarPost (self, a):
+		self.Npost = a
+
+	def InsertarFoto(self,photo):
+		self.photo = photo
+
+	def InsertarCurriculum(self, a, b):
+		self.curriculum = a
+		self.profesion = b
