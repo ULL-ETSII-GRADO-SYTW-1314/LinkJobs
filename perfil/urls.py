28581e78eb854b746898217a812503cdb5b006c6
Javier Mena Mena <alu0100454741@ull.edu.es>
2013-12-17 19:31:39 +0000

    Añadiendo url de las noticias

--- perfil/urls.py
+++ perfil/urls.py
@@ -7,6 +7,7 @@ urlpatterns=patterns('perfil.views',
 	url (r'^(?P<username>\w+)/curriculum/(?P<completado>\w+)/$','subircurriculum',name='vista_curriculum'),
 	url (r'^(?P<username>\w+)/ver_curriculum/$','view_curriculum',name='vista_curriculum'),
 	url (r'^(?P<username>\w+)/perfil/(?P<id>\w+)$','ver_perfil',name='vista_perfil'),
+	url (r'^(?P<username>\w+)/noticias/$','home_noticias',name='vista_noticias'),
 )
 
 
