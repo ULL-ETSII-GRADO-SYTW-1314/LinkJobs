8f6d2e4bcae3f2139fb6d945094d0979a203accb
Javier Mena Mena <alu0100454741@ull.edu.es>
2013-12-17 19:08:53 +0000

    Añandiendo la url de imagen

--- perfil/urls.py
+++ perfil/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 urlpatterns=patterns('perfil.views',
 	url(r'^(?P<username>\w+)/$', 'user_details',name='vista_perfil'),
+	url (r'^(?P<username>\w+)/imagen/$','subirimagen',name='vista_subirImagen'),
 )
 
 
