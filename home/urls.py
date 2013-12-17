44f4e2970230de00411249ee73b4f666f6245bdf
Javier Mena Mena <alu0100454741@ull.edu.es>
2013-12-17 19:57:21 +0000

    Modificado estructura

--- home/urls.py
+++ home/urls.py
@@ -6,11 +6,11 @@ urlpatterns = patterns('home.views',
     url (r'^about/$','about_view',name='vista_about'),
     url (r'^privacity/$','privacity_view',name='vista_privacidad'),
     url (r'^terms_of_use/$','terms_of_use_view',name='vista_terminos'),
-    url (r'^signup/$','signup_view',name='vista_signup'),
-    url (r'^login/$','login_view',name='vista_login'),
     url (r'^contact/$','contact_view',name='vista_contacto'),
     url (r'^faq/$','faq_view',name='vista_faq'),
     url (r'^help/$','help_view',name='vista_ayuda'),
 
-
+    url(r'^logout/$', 'logout',name='vista_logout'),
+    url (r'^signup/$','signup_view',name='vista_signup'),
+    url (r'^login/$','login_view',name='vista_login'),
 )
