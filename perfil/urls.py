fac6728ddb5a481eb874f8813349ae15ed93b8e4
Javier Mena Mena <alu0100454741@ull.edu.es>
2013-12-17 19:09:53 +0000

    Añandiendo la url de curriculum

--- perfil/urls.py
+++ perfil/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 urlpatterns=patterns('perfil.views',
 	url(r'^(?P<username>\w+)/$', 'user_details',name='vista_perfil'),
 	url (r'^(?P<username>\w+)/imagen/$','subirimagen',name='vista_subirImagen'),
+	url (r'^(?P<username>\w+)/curriculum/(?P<completado>\w+)/$','subircurriculum',name='vista_curriculum'),
 )
 
 
