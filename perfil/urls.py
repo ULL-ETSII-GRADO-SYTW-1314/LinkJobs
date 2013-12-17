e1dbdc50bd1b93aa1805fb2c1fdc0474dba59eb2
Javier Mena Mena <alu0100454741@ull.edu.es>
2013-12-17 19:46:13 +0000

    Añadiendo url de borrar post

--- perfil/urls.py
+++ perfil/urls.py
@@ -11,6 +11,7 @@ urlpatterns=patterns('perfil.views',
 	url (r'^(?P<username>\w+)/curriculum/(?P<id>\w+)$','ver_su_curriculum',name='vista_de_curriculum'),
 	url (r'^(?P<username>\w+)/seguir/(?P<id>\w+)$','seguir',name='vista_seguir'),
 	url (r'^(?P<username>\w+)/sigues_a/$','quien_sigues',name='vista_quien_sigues'),
+	url (r'^borrar/(?P<id>\w+)/$','borrar_post',name='vista_borrarpost'),
 )
 
 
